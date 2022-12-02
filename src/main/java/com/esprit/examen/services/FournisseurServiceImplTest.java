7febf35490f9025a786cd68e794e336abcd099ed
Hamza Sarraj <Hamza.Sarraj@onetech-group.corp>
2022-12-02 17:23:51 +0100

    mockitoplugin

--- src/main/java/com/esprit/examen/services/FournisseurServiceImplTest.java
+++ src/main/java/com/esprit/examen/services/FournisseurServiceImplTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.Date;
